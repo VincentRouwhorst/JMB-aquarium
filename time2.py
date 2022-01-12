bdc5f64336ba3363e48a19fff13cbe92f0891bc4
VincentRouwhorst <rouwhorst@yahoo.com>
2022-01-12 19:30:44 +0100

    Small updates

--- time2.py
+++ time2.py
@@ -12,38 +12,38 @@ id_name = {"JMB-C1":      {"idx" : 7297, "lastupdate" : '', "lastlevel" : '', "T
 
 for key in sorted(id_name):
    id_name[key]["lastupdate"] = datetime.now()
-   id_name[key]["lastlevel"] = 0  # test setup
+   id_name[key]["lastlevel"] = 10  # test setup
 
-#T1 = starttime datetime.now()
-#T2 = endtime
-#T1 ------A ------A -------A --------A -------A T2
-#Tnow------------------------------Tnow-------Tnow
-#T2 = Tnow + Tdelta
-
-#SunUp and SunDown
-#StartLevel----A ------A -----A -----A ------EndLevel
-#0-----1------1-----1-----1-----1-----1---- 100
-#0--------------------45--------------------100
-
-#ToGoLevel = EndLevel - lastlevel
+# ---=== SunControl ===---
+# T1 = starttime datetime.now()
+# T2 = endtime
+# T1 ------A ------A -------A --------A -------A T2
+# Tnow------------------------------Tnow-------Tnow
+# T2 = Tnow + Tdelta
+#
+# SunUp and SunDown
+# StartLevel----A ------A -----A -----A ------EndLevel
+# 0-----1------1-----1-----1-----1-----1---- 100
+# 0--------------------45--------------------100
+#
+# ToGoLevel = EndLevel - lastlevel
 #     55   =      100 - 45
-
-#ToGoTime = T2 - Tnow
-
-#TimeNextAction = ToGoTime / ToGoLevel
+#
+# ToGoTime = T2 - Tnow
+#
+# TimeNextAction = ToGoTime / ToGoLevel
 #  1,636363636363636  = 90 / 55
 
 T1 = datetime.now()
-T2 = T1 + timedelta(minutes=1)   #buttonpress 
+T2 = T1 + timedelta(minutes=1)   # at buttonpress settime to finish
 
 def SunControl(id, Sun):
-   global T1
    global T2
    #Sun = Sun Up and Down setting for function switch
    if Sun == "Up":
-      EndLevel = 100
+      EndLevel = 100 # preset Sun Up
    elif Sun == "Down":
-      EndLevel = 0   # preset if Sun Up Down
+      EndLevel = 0   # preset Sun Down
 
    #Lastlevel = id_name[id]["lastlevel"]  # 0 - 100 % | for test 90%
    ToGoLevel = abs(EndLevel - id_name[id]["lastlevel"]) # abs is used for creating positive value
@@ -51,24 +51,23 @@ def SunControl(id, Sun):
    #ActionTimeDelta = ToGoTime.total_seconds() / ToGoLevel
    if ToGoLevel > 0 and ToGoLevel <= 100:  # prevent dev by zero and end of function
       ActionTimeDelta = ToGoTime.total_seconds() / ToGoLevel   # Action Steps in seconds to take
+      #print("ActionTimeDelta : " + str(ActionTimeDelta) + "  Type = " + str(type(ActionTimeDelta)))
+      if id_name[id]["TimeNextAction"] == '':   # fistrun fill 
+         id_name[id]["TimeNextAction"] = datetime.now() + timedelta(seconds=ActionTimeDelta) # for first loop
+         print("---=== hit first run ===---")
+      elif id_name[id]["TimeNextAction"] <= datetime.now():
+         if Sun == "Up":
+            id_name[id]["lastlevel"] += 1
+         elif Sun == "Down":
+            id_name[id]["lastlevel"] -= 1
+         print(str(id) + " : " + str(id_name[id]["lastlevel"]) + " Action : " + str(id_name[id]["TimeNextAction"]))
+         id_name[id]["TimeNextAction"] = datetime.now() + timedelta(seconds=ActionTimeDelta)
+         return id_name[id]["lastlevel"]   # return value extra option for implementation
    else:
       print("---=== End of function ===---")
       quit() # quit at this point
 
-   #print("ActionTimeDelta : " + str(ActionTimeDelta) + "  Type = " + str(type(ActionTimeDelta)))
-   if id_name[id]["TimeNextAction"] == '':   # fistrun fill 
-      id_name[id]["TimeNextAction"] = datetime.now() + timedelta(seconds=ActionTimeDelta) # for first loop
-      print("---=== hit ===---")
-
-   if id_name[id]["TimeNextAction"] <= datetime.now():
-      if Sun == "Up":
-         id_name[id]["lastlevel"] += 1
-      elif Sun == "Down":
-         id_name[id]["lastlevel"] -= 1
-      print(str(id) + " : " + str(id_name[id]["lastlevel"]) + " Action : " + str(id_name[id]["TimeNextAction"]))
-      id_name[id]["TimeNextAction"] = datetime.now() + timedelta(seconds=ActionTimeDelta)
-
 
 while True:
  for key in sorted(id_name):
-    SunControl(key, "Up")
+    SunControl(key, "Down")
